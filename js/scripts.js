0e4a50e518d6ae9d37b3d961bdaf01b9318a96bb
nhu finney <nhufinney@gmail.com>
2015-04-15 16:05:07 -0700

    finish all codes. passed tests. html finished.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,63 @@
-var BankAccount = {
-  balance: 0,
-  withdraw: function(amount) {
-    this.balance = this.balance - amount;
-  },
-  deposit: function(amount) {
-    this.balance = this.balance + amount;
-  }
+var MovieTicket ={
+    user_age: 0,
+    movie_time: 0,
+    ticket_price:0,
+
+    age: function(input_age){
+        if (input_age <=18 || input_age>=60){
+            this.user_age = 0.5;
+        } else{
+            this.user_age = 1;
+        }
+    },
+
+    time: function(chose_time){
+        if (chose_time ===2 ){
+            this.movie_time = 5;
+        } else{
+            this.movie_time = 0;
+        }
+    },
+
+    movie_cost: function(movie_title){
+        if (movie_title === 1){
+            this.ticket_price= 10;
+        }
+
+        if (movie_title === 2){
+            this.ticket_price =12;
+        }
+
+        if (movie_title === 3){
+            this.ticket_price= 14;
+        }
+
+        if (movie_title === 4){
+            this.ticket_price= 15;
+        }
+    },
+
+    price: function(){
+        this.ticket_price = this.ticket_price * this.user_age + this.movie_time;
+    }
 };
-var newMember = Object.create(BankAccount);
+
+var myMovie = Object.create(MovieTicket);
+
 $(document).ready(function() {
+ $("form#price").submit(function(event) {
+     event.preventDefault();
 
+     var input_movie = parseInt($("select").val());
+     var input_time = parseInt($("input#time").val());
+     var input_age = parseInt($("input#age").val());
+     myMovie.age(input_age);
+     myMovie.time(input_time);
+     myMovie.movie_cost(input_movie);
+     myMovie.price();
 
-     $("form#signUp").submit(function(event) {
-       event.preventDefault();
+     debugger;
 
-         var fullname = $("input#name").val();
-         var input_balance = parseInt($("input#balance").val());
-
-
-         newMember.fullname= fullname;
-         newMember.balance = input_balance;
-
-         $(".show-balance").text(newMember.balance);
-    });
-
-
-    $("form#money").submit(function(event) {
-      event.preventDefault();
-
-        var new_deposit = parseInt($("input#deposit").val());
-        var new_withdraw = parseInt($("input#withdraw").val());
-        newMember.deposit(new_deposit);
-        newMember.withdraw(new_withdraw);
-
-        $(".show-balance").text(newMember.balance);
-    });
-
-    
+     $(".show_cost").text(myMovie.ticket_price);
+ });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,74 @@
-describe('BankAccount', function(){
-    describe("deposit", function(){
-        it ("add the amount into the deposit", function(){
-            var testBankAccount = Object.create(BankAccount);
-            testBankAccount.deposit(200);
-            expect(testBankAccount.balance).to.equal(200);
+describe('MovieTicket', function(){
+    describe("age", function(){
+        it ("add a normal age and return no discount", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.age(22);
+            expect(testUser.user_age).to.equal(1);
         });
+    });
 
-    describe("withdraw", function(){
-        it ("subtract the amount from the balance", function(){
-            var testBankAccount = Object.create(BankAccount);
-            testBankAccount.deposit(200);
-            testBankAccount.withdraw(150);
-            expect(testBankAccount.balance).to.equal(50);
+    describe("age", function(){
+        it ("add a matinee age and return to a discount", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.age(10);
+            expect(testUser.user_age).to.equal(0.5);
         });
-    })
+    });
 
+    describe("age", function(){
+        it ("add an age>60 and return to a senior discount", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.age(80);
+            expect(testUser.user_age).to.equal(0.5);
+        });
+    });
+
+    describe("time", function(){
+        it ("add a normal time and return to normal", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.time(14);
+            expect(testUser.movie_time).to.equal(0);
+        });
+    });
+
+    describe("time", function(){
+        it ("add a golden time and return to an extra charge", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.time(20);
+            expect(testUser.movie_time).to.equal(5);
+        });
+    });
+
+    describe("movie_cost", function(){
+        it ("add a movie chosen and return to the price", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.movie_cost(2);
+            expect(testUser.ticket_price).to.equal(12);
+        });
+    });
+
+    describe("price", function(){
+        it ("add a normal user and return to a normal price", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.time(14);
+            testUser.age(24);
+            testUser.movie_cost(2);
+            testUser.price();
+            expect(testUser.price).to.equal(12);
+
+        });
+    });
+
+    describe("price", function(){
+        it ("add a baby age and return to a discount price", function(){
+            var testUser = Object.create(MovieTicket);
+            testUser.time(10);
+            testUser.age(10);
+            testUser.movie_cost(1);
+            testUser.price();
+            expect(testUser.price).to.equal(5);
+
+
+        });
     });
 });
